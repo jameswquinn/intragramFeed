a5465467eefa4a30aa9dae1a002f8dbf7d1a3b7c
jameswquinn <jameswquinn@me.com>
2017-08-14 11:14:37 +0000

    update

--- assets/script/main.js
+++ assets/script/main.js
@@ -14,18 +14,4 @@ var feed = new Instafeed({
 		}
 });
 
-window.onload = function() {
-	feed.run();
 
-	var _gauges = _gauges || [];
-	(function() {
-		var t   = document.createElement('script');
-		t.type  = 'text/javascript';
-		t.async = true;
-		t.id    = '';
-		t.setAttribute('data-site-id', '4d5aa2a95ffe1f587b000001');
-		t.src = '//secure.gaug.es/track.js';
-		var s = document.getElementsByTagName('script')[0];
-		s.parentNode.insertBefore(t, s);
-	})();
-};
