fc21aa9d58c102bc0025fc7dc67a28e85d947007
jameswquinn <jameswquinn@me.com>
2017-08-14 07:10:16 +0100

    play                                                                                                    update

--- assets/script/main.js
+++ assets/script/main.js
@@ -13,21 +13,3 @@ var feed = new Instafeed({
 				el.className += ' ' + 'show';
 		}
 });
-
-/*
-window.onload = function() {
-	feed.run();
-
-	var _gauges = _gauges || [];
-	(function() {
-		var t   = document.createElement('script');
-		t.type  = 'text/javascript';
-		t.async = true;
-		t.id    = '';
-		t.setAttribute('data-site-id', '4d5aa2a95ffe1f587b000001');
-		t.src = '//secure.gaug.es/track.js';
-		var s = document.getElementsByTagName('script')[0];
-		s.parentNode.insertBefore(t, s);
-	})();
-};
-*/
