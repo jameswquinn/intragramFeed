d06bd46362aa3e0cfd2083e47ad4ef17d9a1e683
jameswquinn <jameswquinn@me.com>
2017-08-14 11:48:09 +0000

    update

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,7 +2,7 @@ var feed = new Instafeed({
 		get: 'user',
 		userId: 1280170021,
 		accessToken: '1280170021.1677ed0.e9a6e8b385dc4002a5218422f3c5bb64',
-        template: '<a href="{{link}}"><span>{{caption}}</span><img class="set" src="{{image}}" /></a>',
+        template: '<a href="{{link}}"><span>{{caption}}{{comments}}{{likes}}</span><img class="set" src="{{image}}" /></a>',
 		target: 'instagram',
 		resolution: 'standard_resolution',
 		after: function() {}
